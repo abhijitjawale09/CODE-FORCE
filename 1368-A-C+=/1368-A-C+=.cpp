46e2d551998af7606edc008dbf83e4056752fca8
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-11-02 21:55:17 +0530

    Create 1368-A-C+=.cpp

---
+++ 1368-A-C+=/1368-A-C+=.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long  a , b, n , sum , c;
+     cin>>a>>b>>n;
+         sum = 0,c = 0;
+        while(sum <= n){
+            if(a > b){
+                b += a;
+                sum = b;
+                c++;
+            }
+            else{
+                a += b;
+                sum = a;
+                c++;
+            }
+            if(sum > n){
+                break;
+            }
+        }
+        cout<<c<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
