83a892d349baa81fc3c303e56427dabc9db86412
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-12-20 13:09:39 +0530

    Create 0476-A-Dreamoon and Stairs.cpp

---
+++ 0476-A-Dreamoon and Stairs/0476-A-Dreamoon and Stairs.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define nl "\n"
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld PI = acos(-1.0);
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+//----------------------------------Solution code here-----------------------------//
+void abhijitjawale09() {
+    int n, m;
+    cin >> n >> m;
+
+    int total = (n + 1) / 2;  
+
+    while (total % m != 0) {
+        total++;
+    }
+
+    if (total > n) {
+        cout << -1 << endl;
+    } else {
+        cout << total << endl;
+    }
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        abhijitjawale09();
+    }
+    return 0;
+}
