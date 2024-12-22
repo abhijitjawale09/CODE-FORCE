d3e375ad3c46c1c5311ed90140c22129b6f08d93
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2024-12-22 12:29:40 +0530

    Create 0486-B-OR in Matrix.cpp

---
+++ 0486-B-OR in Matrix/0486-B-OR in Matrix.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define nl "\n"
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld PI = acos(-1.0);
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+
+//----------------------------------Solution code here-----------------------------//
+void abhijitjawale09() {
+    int n, m;
+    cin >> m >> n;
+    int B[m][n], A[m][n], C[m][n];
+    for (int i = 0; i < m; ++i)
+    {
+    	for (int j = 0; j < n; ++j)
+    	{
+    		cin >> B[i][j];
+    		A[i][j] = 1;
+    		C[i][j] = 0;
+    	}
+    }
+    for (int i = 0; i < m; ++i)
+    {
+    	for (int j = 0; j < n; ++j)
+    	{
+    		if (B[i][j] == 0)
+    		{
+    			for (int k = 0; k < n; ++k)
+    			{
+    				A[i][k] = 0;
+    			}
+    			for (int k = 0; k < m; ++k)
+    			{
+    				A[k][j] = 0;
+    			}
+    		}
+    	}
+    }
+    for (int i = 0; i < m; ++i)
+    {
+    	for (int j = 0; j < n; ++j)
+    	{
+    		if (A[i][j])
+    		{
+    			for (int k = 0; k < n; ++k)
+    			{
+    				C[i][k] = 1;
+    			}
+    			for (int k = 0; k < m; ++k)
+    			{
+    				C[k][j] = 1;
+    			}
+    		}
+    	}
+    }
+    for (int i = 0; i < m; ++i)
+    {
+    	for (int j = 0; j < n; ++j)
+    	{
+    		if (C[i][j] != B[i][j])
+    		{
+    			cout <<"NO\n";
+    			return;
+    		}
+    	}
+    }
+    cout <<"YES\n";
+    for (int i = 0; i < m; ++i)
+    {
+    	for (int j = 0; j < n; ++j)
+    	{
+    		cout << A[i][j] <<" ";
+    	}
+    	cout << "\n";
+    }
+    
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        abhijitjawale09();
+    }
+    return 0;
+}      
