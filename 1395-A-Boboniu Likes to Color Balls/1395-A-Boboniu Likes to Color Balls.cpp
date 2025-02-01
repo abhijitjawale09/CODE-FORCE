c3dae111196c4c530e77b8df84c9d383a48ce65a
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2025-02-01 23:06:31 +0530

    Create 1395-A-Boboniu Likes to Color Balls.cpp

---
+++ 1395-A-Boboniu Likes to Color Balls/1395-A-Boboniu Likes to Color Balls.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define nl "\n"
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld PI = acos(-1.0);
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+
+//----------------------------------Solution code here-----------------------------//
+void abhijitjawale09() {
+    ll r,g,b,w;
+    cin>>r>>g>>b>>w;
+    
+    ll odd =  0;
+    if(r%2) odd++;
+    if(g%2) odd++;
+    if(b%2)odd++;
+    if(w%2)odd++;
+    
+    if(odd ==2 || odd>=3  && (r==0 || g == 0 || b == 0)) cout<<"NO"<<endl;
+    else{
+        cout<<"YES"<<endl;
+    }
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        abhijitjawale09();
+    }
+    return 0;
+}      
